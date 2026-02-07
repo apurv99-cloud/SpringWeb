08fb84916aa5ab924343aeea0da831d96398056a
Apurv <sinhaapurv99@gmail.com>
2026-02-07 13:55:34 +0530

    Worke with ModelAndView

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,30 +1,35 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "index.jsp"; 
-        // to display the .jsp we have add dependency TOMCAT JASPER, as SpringBoot personally don't supports .jsp file
+        return "index";
+        // to display the .jsp we have add dependency TOMCAT JASPER, as SpringBoot
+        // personally don't supports .jsp file
 
     }
 
     @RequestMapping("add")
-    // DespatcherServlet will automatically mapp without doing manually.
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    // DespatcherServlet will automatically map without doing manually.
+    // public String add(int num1, int num2, HttpSession session) {
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv) {
+        // int num1 = Integer.parseInt(req.getParameter("num1"));
+        // int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
-        session.setAttribute("result", result); // Adding data in session object
+        // session.setAttribute("result", result); // Adding data in session object
+        // model.addAttribute("result", result);
+        mv.addObject("result", result);
+        mv.setViewName("result");
         System.out.println(result);
-        return "result.jsp";
+        return mv;
     }
 
 }
---
+++ demo/src/main/webapp/views/result.jsp
@@ -0,0 +1,12 @@
+<%@page language="java" %>
+<html>
+  <body>
+    <!--JSP FILE GIVE US DEFAULT ACCESS TO SESSION, REQ AND RES OBJECT-->
+
+    <!--This is Servlet Way-->
+    <!-- <h2>Result is: <%= session.getAttribute("result") %> </h2> -->
+
+     <!-- THIS IS JSTL(JSP STANDARD LIBRARY) way -->
+     <h2>Result is: ${result} </h2>
+  </body>
+</html>
